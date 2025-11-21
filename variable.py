dba0a603481c4d4b0714d5d8fbbebee60b91d516
Reuben170070 <reubenaby07@gmail.com>
2025-11-20 18:27:00 -0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,8 @@
+# Program Description : This is a self-intro program
+#Varibles that store name, age and favorite food
+name = "Reuben"
+age = 22
+favfood = "Pizza"
+
+#Displaying all the information stored to user. 
+print(f"My name is {name}. I am {age} years old. My favorite food is {favfood}." )
